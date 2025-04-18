6a3a6e99ab2a5442c59bf7fca7db9d26ce84e1e1
Sérgio Soriano <sergio.soriano@ufpe.br>
2025-04-18 10:25:39 -0300

    Update extrator_ofx.py

--- extrator_ofx.py
+++ extrator_ofx.py
@@ -5,19 +5,23 @@ def extrair_dados_ofx(ofx_file_path):
     """
     Extrai dados de transações financeiras de um arquivo OFX e os converte para um DataFrame.
     """
-    with open(ofx_file_path, 'r') as ofx_file:
-        ofx = OfxParser.parse(ofx_file)
-    
-    # Extração das transações de cartão de crédito ou conta corrente
-    transacoes = []
-    for transaction in ofx.account.statement.transactions:
-        transacoes.append({
-            'Data': transaction.date,
-            'Descricao': transaction.name,
-            'Valor': transaction.amount
-        })
-    
-    # Cria um DataFrame com as transações extraídas
-    df = pd.DataFrame(transacoes)
-    
-    return df
+    try:
+        # Lê o arquivo OFX
+        with open(ofx_file_path, 'r') as ofx_file:
+            ofx = OfxParser.parse(ofx_file)
+        
+        # Extração das transações de cartão de crédito ou conta corrente
+        transacoes = []
+        for transaction in ofx.account.statement.transactions:
+            transacoes.append({
+                'Data': transaction.date,
+                'Descricao': transaction.name,
+                'Valor': transaction.amount
+            })
+        
+        # Cria um DataFrame com as transações extraídas
+        df = pd.DataFrame(transacoes)
+        return df
+    except Exception as e:
+        print(f"Erro ao ler o arquivo OFX: {e}")
+        return pd.DataFrame()  # Retorna um DataFrame vazio em caso de erro
